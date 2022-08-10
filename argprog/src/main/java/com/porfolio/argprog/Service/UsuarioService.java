bb3897a15dcfa43bb6a1889e857c964197e08e5e
Juanma469 <jmbasualdo05@gmail.com>
2022-08-10 18:10:32 -0300

    .

--- argprog/src/main/java/com/porfolio/argprog/Service/UsuarioService.java
+++ argprog/src/main/java/com/porfolio/argprog/Service/UsuarioService.java
@@ -1,13 +1,41 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.porfolio.argprog.Service;
 
-/**
- *
- * @author Juan Manuel
- */
+import com.porfolio.argprog.Entity.Usuario;
+import com.porfolio.argprog.Repository.UsuarioRepo;
+import java.util.List;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional 
 public class UsuarioService {
+    private final UsuarioRepo usuarioRepo;
+    
+    @Autowired
+    public UsuarioService (UsuarioRepo usuarioRepo){
+        this.usuarioRepo = usuarioRepo;
+    }
+    
+    public Usuario  addUsuario (Usuario usuario){
+        return usuarioRepo.save(usuario);
+    }
+    
+    public List<Usuario> buscarUsuario(){
+        return usuarioRepo.findAll();
+    }
+    
+    
+    public Usuario editarUsuario(Usuario usuario){
+        return usuarioRepo.save(usuario);
+    }
+    
+    public void borrarUsuario(Long id){
+        usuarioRepo.deleteById(id);
+    }
+    
+    
+    
     
 }
